d899909006c7dc083fb71e13c10152b0326b0e4a
vmysh <73319790+vmysh@users.noreply.github.com>
2021-03-25 21:17:24 -0400

    express-api code

---
+++ express-api/app.js
@@ -0,0 +1,28 @@
+//Require packages and set the port
+
+const routes = require('./routes/routes');
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = 3002;
+const app = express();
+
+//Use Node.js body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+// app.get('/', (request, response) => {
+//     console.log(`URL: ${request.url}`);
+//     response.send({message: 'Node.js and Express REST API'});
+// });
+
+routes(app);
+
+//Start the server
+
+const server = app.listen(port, (error) => {
+    if (error) return console.log(`Error: ${error}`);
+
+    console.log(`Server listening on port ${server.address().port}`);
+});
---
+++ express-api/hello-server.js
@@ -0,0 +1,18 @@
+//Build a server with Node's HTTP module
+
+const http = require('http');
+const port = 3001;
+const server = http.createServer();
+
+server.on('request', (request, response) => {
+    console.log(`URL: ${request.url}`);
+    response.end('Hello, server!')
+})
+
+//Start the server
+
+server.listen(port, (error) => {
+    if (error) return console.log(`Error: ${error}`);
+
+    console.log(`Server is listening on port ${port}`)
+})
